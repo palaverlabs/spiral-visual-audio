ef8ba6cccede4ac454802bd59ba0ea77bec48531
palaverlabs <will@palaverlabs.com>
2026-02-18 12:55:47 -0700

    Add server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  let filePath = req.url === '/' ? '/index.html' : req.url;
+  filePath = path.join(__dirname, filePath);
+  
+  const ext = path.extname(filePath);
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'application/javascript',
+    '.css': 'text/css',
+    '.svg': 'image/svg+xml',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+  };
+  
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+      return;
+    }
+    res.writeHead(200, {
+      'Content-Type': mimeTypes[ext] || 'application/octet-stream',
+      'Cache-Control': 'no-cache'
+    });
+    res.end(content);
+  });
+});
+
+server.listen(5000, '0.0.0.0', () => {
+  console.log('Server running on http://0.0.0.0:5000');
+});
